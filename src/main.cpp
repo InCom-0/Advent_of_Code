f7ece6fdbf4042832c537b921b258603ef270f8e
Michal Lurie <Michal.Lurie@gmail.com>
2024-12-07 19:59:12 +0100

    Added 2019 21 part 1

--- src/main.cpp
+++ src/main.cpp
@@ -21,9 +21,9 @@ Note that this main is used while developing the solutions.
 
 int main() {
 
-    auto pth_1 = std::filesystem::path(DATAFOLDER_2019) / "2019_20_1.txt";
+    auto pth_1 = std::filesystem::path(DATAFOLDER_2019) / "2019_21_1.txt";
 
-    std::cout << AOC2019::day20_2(pth_1.generic_string()) << '\n';
+    std::cout << AOC2019::day21_1(pth_1.generic_string()) << '\n';
 
 
     return 1;
--- src/problems/AOC_2019/AOC_2019_day_21.h
+++ src/problems/AOC_2019/AOC_2019_day_21.h
@@ -3,4 +3,6 @@
 #include <string>
 
 namespace AOC2019 {
+    long long day21_1(std::string dataFile);
+    long long day21_2(std::string dataFile);
 } // namespace AOC2018
