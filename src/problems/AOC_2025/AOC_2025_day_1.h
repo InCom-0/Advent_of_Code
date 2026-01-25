13ffb4d9656364a1029b63869a07b22323a4a218
Michal Lurie <Michal.Lurie@gmail.com>
2026-01-25 01:05:45 +0100

    Feat: Finished the 2025 prep

--- src/problems/AOC_2025/AOC_2025_day_1.h
+++ src/problems/AOC_2025/AOC_2025_day_1.h
@@ -4,4 +4,9 @@
 
 namespace AOC2025 {
 
-} // namespace AOC2024
+long long
+day1_1(std::string dataFile);
+long long
+day1_2(std::string dataFile);
+
+} // namespace AOC2025
---
+++ test/problems/AOC_2025_test.cpp
@@ -0,0 +1,20 @@
+#include <gtest/gtest.h>
+
+#include <filesystem>
+
+#include <AOC_2025.h>
+#include <incom_commons.h>
+
+#define DATAFOLDER      "../../data"
+#define DATAFOLDER_2024 "../../data/AOC_2025"
+using namespace std::literals;
+
+
+TEST(A_2024_d1_1, A1) {
+    auto pth = std::filesystem::path(DATAFOLDER_2024) / "2024_1_1.txt";
+    EXPECT_EQ(2430334, AOC2025::day1_1(pth.generic_string()));
+}
+TEST(A_2024_d1_2, A1) {
+    auto pth = std::filesystem::path(DATAFOLDER_2024) / "2024_1_1.txt";
+    EXPECT_EQ(28786472, AOC2025::day1_2(pth.generic_string()));
+}
