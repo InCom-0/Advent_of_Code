191c15d7438344c624e6faf4f5dc7618c37f9167
Michal Lurie <Michal.Lurie@gmail.com>
2026-04-11 00:45:10 +0200

    Feat: developing 'tetris solver' pt.2

--- src/problems/AOC_2025/AOC_2025_day_12.cpp
+++ src/problems/AOC_2025/AOC_2025_day_12.cpp
@@ -3,11 +3,12 @@
 #include <ctre.hpp>
 #include <flux.hpp>
 
-#include <ranges>
+#include <incstd/core/hashing.hpp>
 #include <string>
 
-#include <incom_commons.h>
 #include <AOC_2025_solvers.h>
+#include <incom_commons.h>
+
 
 namespace AOC2025 {
 size_t
@@ -16,64 +17,16 @@ day12_1_trial(std::string dataFile) {
     auto d_ctre   = ctre::search<R"(\d+)">;
     auto input    = incom::aoc::parseInputUsingCTRE::processFile(dataFile, any_ctre).front();
 
-    struct Shape {
-        std::vector<std::array<std::array<bool, 5>, 5>> matrices;
+    namespace inctetrix = incom::aoc::solvers::tetris;
 
-        std::pair<Shape, size_t>
-        overlayWith(Shape const &other) {
+    inctetrix::Shape<5>  arc;
+    inctetrix::Solver<5> rr;
 
+    auto tt = rr.getOrCompute(arc);
 
-            return {};
-        }
-    };
+    ankerl::unordered_dense::set<inctetrix::Shape<5>, incom::standard::hashing::XXH3Hasher> mp;
 
-    struct Tree {
-        int yDim;
-        int xDim;
-
-        std::vector<std::vector<char>> spots;
-
-
-        std::vector<Shape>  shapesToUse;
-        std::vector<size_t> reqdShapes;
-
-        Tree(size_t ySize, size_t xSize, std::vector<Shape> shapesToUse, std::vector<size_t> perShapeCountReq,
-             size_t windowsSz_y, size_t windowsSz_x)
-            : yDim(std::move(ySize)), xDim(std::move(xSize)),
-              spots(std::vector(yDim + 2, std::vector<char>(xDim + 2, 0))), shapesToUse(std::move(shapesToUse)) {
-            // 'Fake' border of occupied spots at construction to make later computations easier
-            spots.front() = std::vector<char>(spots.front().size(), 1);
-            for (auto &spotLine : std::views::drop(std::views::take(spots, spots.size() - 1), 1)) {
-                spotLine.front() = 1;
-                spotLine.back()  = 1;
-            }
-            spots.back() = std::vector<char>(spots.front().size(), 1);
-        }
-    };
-
-    std::vector<Shape> shapes;
-    std::vector<Tree>  trees;
-
-    for (size_t lineID = 0; lineID < input.size(); ++lineID) {
-        if (input.at(lineID).size() == 2) {
-            shapes.emplace_back();
-            shapes.back().matrices.emplace_back();
-            for (size_t shape_line = 0; shape_line < 3; ++shape_line) {
-                lineID++;
-                for (int chrID = 0; auto oneChr : input.at(lineID)) {
-                    shapes.back().matrices.front().at(shape_line).at(chrID++) = (oneChr == '#' ? true : false);
-                }
-            }
-        }
-        if (input.at(lineID).size() > 5) {
-            auto prsRes = incom::aoc::parseInputUsingCTRE::processOneLine(input.at(lineID), d_ctre, d_ctre, d_ctre,
-                                                                          d_ctre, d_ctre, d_ctre, d_ctre, d_ctre);
-            trees.push_back(Tree(std::stoi(prsRes.at(0)), std::stoi(prsRes.at(1)), shapes,
-                                 {std::stoull(prsRes.at(2)), std::stoull(prsRes.at(3)), std::stoull(prsRes.at(4)),
-                                  std::stoull(prsRes.at(5)), std::stoull(prsRes.at(6)), std::stoull(prsRes.at(7))},
-                                 5, 5));
-        }
-    }
+    mp.insert(arc);
 
 
     return 0uz;
--- src/problems/AOC_2025/include/AOC_2025_solvers.h
+++ src/problems/AOC_2025/include/AOC_2025_solvers.h
@@ -1,7 +1,11 @@
 #pragma once
 
+#include <algorithm>
 #include <ankerl/unordered_dense.h>
 #include <cassert>
+#include <functional>
+#include <optional>
+#include <utility>
 #include <vector>
 
 #include <ctre.hpp>
@@ -18,29 +22,73 @@ namespace aoc {
 namespace solvers {
 namespace tetris {
 
+
 template <size_t SQSZ>
-requires(SQSZ > 1)
+requires(SQSZ > 2)
 struct Shape {
+    struct OverlayRes {
+        Shape res_shp;
+
+        size_t pointsAdded;
+        size_t pointsOverlaid;
+        size_t pointTouching;
+    };
+
     std::array<std::array<bool, SQSZ>, SQSZ> m_matrix = {};
 
-    // 1) Resulting shape, 2) Points newly added, 3) Points 'overlaid'
-    std::tuple<Shape, size_t, size_t>
-    overlayWith(Shape const &other) {
-        std::tuple<Shape, size_t, size_t> res{{}, 0, 0};
+    // Construction
+    Shape()                 = default;
+    Shape(Shape const &src) = default;
+    Shape(Shape &&src)      = default;
+    ~Shape()                = default;
+
+    Shape &
+    operator=(Shape const &) = default;
+    Shape &
+    operator=(Shape &&) = default;
+
+    bool
+    operator==(Shape const &other) const = default;
+
+
+    Shape(std::array<std::array<bool, SQSZ - 2>, SQSZ - 2> const &src) {
+        for (auto r = 1uz; auto const &line : src) {
+            for (auto c = 1uz; bool one : line) { m_matrix[r][c++] = one; }
+            r++;
+        }
+    }
+    Shape(std::array<std::array<bool, SQSZ>, SQSZ> const &src) : m_matrix(src) {}
+
+    OverlayRes
+    compute_overlayWith(Shape const &other) const {
+        OverlayRes res{{}, 0uz, 0uz, 0uz};
         for (size_t r = 0; r < SQSZ; ++r) {
             for (size_t c = 0; c < SQSZ; ++c) {
-                std::get<2>(res)       += (m_matrix[r][c] and other[r][c]);
-                std::get<1>(res)       += m_matrix[r][c] xor other[r][c];
-                std::get<0>(res)[r][c]  = m_matrix[r][c] | other[r][c];
+                res.pointsOverlaid         += (m_matrix[r][c] and other.m_matrix[r][c]);
+                res.pointsAdded            += (not m_matrix[r][c]) and other.m_matrix[r][c];
+                res.res_shp.m_matrix[r][c]  = m_matrix[r][c] | other.m_matrix[r][c];
             }
         }
+        for (size_t r = 1; r < SQSZ - 1; ++r) {
+            for (size_t c = 1; c < SQSZ - 1; ++c) {
+                if (other.m_matrix[r][c]) {
+                    res.pointTouching += m_matrix[r - 1][c] & (not other.m_matrix[r - 1][c]);
+                    res.pointTouching += m_matrix[r][c - 1] & (not other.m_matrix[r][c - 1]);
+                    res.pointTouching += m_matrix[r][c + 1] & (not other.m_matrix[r][c + 1]);
+                    res.pointTouching += m_matrix[r + 1][c] & (not other.m_matrix[r + 1][c]);
+                }
+            }
+        }
+
         return res;
     }
+
+    std::vector<Shape>
+    compute_alternsRotFlip() const {}
 };
 
 // ADL for hashing using XXH3Hasher
 template <size_t SQSZ>
-requires(SQSZ > 1)
 constexpr void
 XXH3Hash(Shape<SQSZ> const &input, XXH3_state_t *state) {
     XXH3_64bits_update(state, input.m_matrix.data(), sizeof(decltype(input.m_matrix)) * input.m_matrix.size());
@@ -49,14 +97,15 @@ XXH3Hash(Shape<SQSZ> const &input, XXH3_state_t *state) {
 template <size_t SQSZ>
 class Solver {
 public:
-    using Shape_t = Shape<SQSZ>;
-
     struct Pos {
         size_t y = 0;
         size_t x = 0;
     };
+    using Shape_t   = Shape<SQSZ>;
+    using PastRes_t = std::pair<Pos, typename Shape_t::OverlayRes>;
 
-    std::vector<std::pair<Pos, Shape_t>> borderSpaces = {{{0, 0}, {}}};
+
+    std::vector<std::pair<Pos, Shape_t>> frontierTiles = {{{0, 0}, {}}};
 
 
 private:
@@ -65,25 +114,67 @@ private:
     std::vector<size_t>               useableCount_perShape;
 
     // Memoization of what 'shapes_alterns'
-    ankerl::unordered_dense::map<Shape_t, std::vector<std::tuple<Shape_t, size_t, size_t>>> pastResults;
+    ankerl::unordered_dense::map<Shape_t, std::vector<PastRes_t>, incom::standard::hashing::XXH3Hasher> pastComputed;
+
+
+    std::optional<std::reference_wrapper<std::vector<PastRes_t>>>
+    get_computedFor(Shape_t const &tile) {
+        if (auto found = pastComputed.find(tile); found != pastComputed.end()) { return std::ref(found->second); }
+        return std::nullopt;
+    }
 
 
 public:
     Solver() {}
     Solver(std::vector<Shape_t> const &shps, std::vector<std::vector<Shape_t>> const &shps_alterns,
-           ankerl::unordered_dense::map<Shape_t, std::vector<std::tuple<Shape_t, size_t, size_t>>> const &pastReslts)
-        : shapes(shps), shapes_alterns(shps_alterns), pastResults(pastReslts) {}
+           ankerl::unordered_dense::map<Shape_t, std::vector<PastRes_t>> const &pastReslts)
+        : shapes(shps), shapes_alterns(shps_alterns), pastComputed(pastReslts) {}
 
 
     // Returns true if this shape is actually new, False if it is the same as some other existing shape
     bool
-    addShape(Shape<5> toAdd) {
+    addShape(Shape_t const &toAdd) {
         return true;
     }
 
     Solver
-    clone_keepingShapeData() {
-        return Solver(shapes, shapes_alterns, pastResults);
+    clone_keepShapeData() {
+        return Solver(shapes, shapes_alterns, pastComputed);
+    }
+
+    std::vector<PastRes_t> &
+    computeFor(Shape_t const &tile) {
+        std::vector<PastRes_t> resToMap;
+        auto allowed = [](PastRes_t const &toCheck) -> bool { return (toCheck.second.pointsOverlaid == 0uz); };
+
+        for (size_t shpID = 0; shpID < shapes_alterns.size(); ++shpID) {
+            for (size_t alternID = 0; alternID < shapes_alterns.at(shpID).size(); ++alternID) {
+                auto rs =
+                    PastRes_t{Pos{shpID, alternID}, tile.compute_overlayWith(shapes_alterns.at(shpID).at(alternID))};
+                if (allowed(rs)) { resToMap.push_back(rs); }
+            }
+        }
+        std::ranges::sort(resToMap, [](auto const &l, auto const &r) {
+            return ((l.second.pointsAdded + l.second.pointTouching) > (r.second.pointsAdded + r.second.pointTouching));
+        });
+        auto insRes = pastComputed.insert({tile, resToMap});
+        return insRes.first->second;
+    }
+
+    std::vector<PastRes_t> &
+    getOrCompute(Shape_t const &tile) {
+        if (auto comp = get_computedFor(tile); comp.has_value()) { return comp.value(); }
+        return computeFor(tile);
+    }
+
+
+    Shape_t
+    solve_oneStep() {
+        std::vector<PastRes_t> possibilties;
+
+        for (auto const &oneFT : frontierTiles) {
+
+        }
     }
 };
 } // namespace tetris
