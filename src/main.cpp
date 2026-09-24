96a98d5d43b67d9505a4e5bd4ff5a98ff0c4d9a8
Michal Lurie <Michal.Lurie@gmail.com>
2026-09-25 00:32:31 +0200

    Added 2023 day 4

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,9 @@ main() {
 
     // auto pth_1    = std::filesystem::path(DATAFOLDER_2019) / "2019_25_1.txt";
     // auto pth_2024 = std::filesystem::path(DATAFOLDER_2024) / "2024_25_1.txt";
-    auto pth_2019 = std::filesystem::path(DATAFOLDER_2023) / "2023_3_1.txt";
+    auto pth_2019 = std::filesystem::path(DATAFOLDER_2023) / "2023_4_1.txt";
 
-    std::cout << AOC2023::day3_2(pth_2019.generic_string()) << '\n';
+    std::cout << AOC2023::day4_2(pth_2019.generic_string()) << '\n';
 
 
     return 1;
--- src/problems/AOC_2023/AOC_2023_day_4.cpp
+++ src/problems/AOC_2023/AOC_2023_day_4.cpp
@@ -1,11 +1,78 @@
+#include <algorithm>
 #include <ankerl/unordered_dense.h>
+#include <cstddef>
 #include <ctre.hpp>
 #include <flux.hpp>
+#include <functional>
 #include <incom_commons.h>
+#include <iostream>
 #include <ranges>
+#include <span>
 #include <string>
 
 
-namespace AOC2025 {
+namespace AOC2023 {
 
-} // namespace AOC2025
+size_t
+day4_1(std::string dataFile) {
+    auto w_ctre = ctre::search<R"(\d+)">;
+    auto input  = incom::aoc::parseInputUsingCTRE::processFileRPT(dataFile, w_ctre);
+
+    auto const vals =
+        std::views::transform(input,
+                              [](auto const &line) {
+                                  return std::views::transform(line, [](auto const &val) { return std::stoull(val); }) |
+                                         std::ranges::to<std::vector>();
+                              }) |
+        std::ranges::to<std::vector>();
+
+    size_t totalRes = 0uz;
+    for (auto const &valLine : vals) {
+        auto const winningView = std::span(valLine.begin() + 1, valLine.begin() + 11);
+        size_t     locRes      = 0uz;
+
+        for (auto const &ticketVal : std::span(valLine.begin() + 11, valLine.end())) {
+            if (std::ranges::find(winningView, ticketVal) != winningView.end()) {
+                locRes *= 2;
+                locRes += (locRes == 0uz);
+            }
+        }
+        totalRes += locRes;
+    }
+
+    return totalRes;
+}
+
+size_t
+day4_2(std::string dataFile) {
+    auto w_ctre = ctre::search<R"(\d+)">;
+    auto input  = incom::aoc::parseInputUsingCTRE::processFileRPT(dataFile, w_ctre);
+
+    auto const vals =
+        std::views::transform(input,
+                              [](auto const &line) {
+                                  return std::views::transform(line, [](auto const &val) { return std::stoull(val); }) |
+                                         std::ranges::to<std::vector>();
+                              }) |
+        std::ranges::to<std::vector>();
+
+    std::vector cardsCounts(vals.size(), 1uz);
+
+    for (size_t valsID = 0uz; auto const &valLine : vals) {
+        auto const winningView = std::span(valLine.begin() + 1, valLine.begin() + 11);
+        size_t     winNumCount = 0uz;
+
+        for (auto const &ticketVal : std::span(valLine.begin() + 11, valLine.end())) {
+            if (std::ranges::find(winningView, ticketVal) != winningView.end()) { winNumCount++; }
+        }
+
+        for (size_t incrementID = valsID + 1; incrementID < (valsID + 1 + winNumCount); ++incrementID) {
+            cardsCounts.at(incrementID) += cardsCounts.at(valsID);
+        }
+
+        valsID++;
+    }
+
+    return std::ranges::fold_left(cardsCounts, 0uz, std::plus{});
+}
+} // namespace AOC2023
